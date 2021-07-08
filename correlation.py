7154589989c64b2bd02ecdacf112d87269d3f6ea
saadsakib <saadsakib3@gmail.com>
2021-07-08 21:12:30 +0600

    Compute correlation

---
+++ correlation.py
@@ -0,0 +1,40 @@
+from data_organizer import DataOrganizer
+from constants import CNL, TL, RL 
+
+class Correlation:
+    def kmer_corr(self, library_name: str):
+        libraries = {
+            'train': [library_name],
+            'test': [], 
+            'train_test': [],
+            'seq_start_pos': 1,
+            'seq_end_pos': 50
+        }
+        
+        for k in [2, 3, 4]:
+            options = {
+                'k_list': [k],
+                'range_split': None,
+                'binary_class': None, 
+                'balance': None
+            }
+
+            organizer = DataOrganizer(libraries, None, None, options)
+            kmer_df = organizer._get_kmer_count()['train'][0]
+            kmer_df = kmer_df.drop(columns=['Sequence #', 'Sequence'])
+            C0_corr = kmer_df.corr()['C0']
+            C0_corr.sort_values(ascending=False).to_csv(
+                f'data/generated_data/correlation/{library_name}_{k}_corr.tsv', sep='\t', index=True)
+
+    def hel_corr():
+        libraries = {
+            'train': [CNL],
+            'test': [TL], 
+            'train_test': [],
+            'seq_start_pos': 1,
+            'seq_end_pos': 50
+        }
+
+        organizer = DataOrganizer(libraries, None, None, None)
+        hel_dfs = organizer._get_helical_sep()
+        pass
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from occurence import Occurence
-from util import append_reverse_compliment, HelicalSeparationCounter
+from util import append_reverse_compliment
 from shape import find_all_shape_values
 from reader import DNASequenceReader
-from constants import RL
+from constants import RL, CNL, TL
+from correlation import Correlation
 
-if __name__ == '__main__':
+
+def plotting_boxplot():
     reader = DNASequenceReader()
     all_lib = reader.get_processed_data()
     lib = RL
     df = append_reverse_compliment(all_lib[RL])
     Occurence().plot_boxplot(df, lib)
+
+
+if __name__ == '__main__':
+    corr = Correlation()
+    corr.kmer_corr(RL)
+    corr.kmer_corr(CNL)
+    corr.kmer_corr(TL)
