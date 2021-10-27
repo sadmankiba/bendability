dd021fa6273c86c18ef159cbfb4362c2af2c8bd4
saadsakib <saadsakib3@gmail.com>
2021-10-27 20:18:04 +0600

    Use perc

--- src/motif/motifs.py
+++ src/motif/motifs.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
+from matplotlib import colors
 import pandas as pd
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 from util.util import FileSave, PathObtain
@@ -18,12 +20,14 @@ class Motifs:
         self._contrib_score_file = (
             f"{PathObtain.input_dir()}/motifs/contribution_scores_model30_train9.txt"
         )
+        self._BEST_MATCH_PERC = 25
         self._motifs = self._read_tomtom_motifs()
         self._contrib_scores = self._read_contrib_scores()
     
     def plot_ranked_tf(self):
         tfdf = self.ranked_tf()
         tfdf = tfdf.drop(np.arange(10, len(tfdf) - 10))
+        mpl.rcParams.update({'font.size': 18})
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
 
@@ -41,7 +45,7 @@ class Motifs:
         plt.bar(tfdf[TF], tfdf[CONTRIB_SCORE])
         plt.ylabel("Average Contribution Score")
         plt.setp(ax.get_xticklabels(), rotation=90, va="top")
-        return FileSave.figure_in_figdir("motifs/ranked_tf.png")
+        return FileSave.figure_in_figdir(f"motifs/ranked_tf_perc_{self._BEST_MATCH_PERC}.png")
 
     def ranked_tf(self) -> pd.DataFrame:
         mdf = pd.merge(
@@ -57,16 +61,19 @@ class Motifs:
 
     def _read_tomtom_motifs(self) -> pd.DataFrame[MOTIF_ID:int, TF:str]:
         df = pd.read_csv(
-            self._tomtom_motif_file, sep="\t", usecols=["Query_ID", "Target_ID"]
+            self._tomtom_motif_file, sep="\t", usecols=["Query_ID", "Target_ID", "p-value"]
         )
         df = df.iloc[:-3]  # Remove comments
+        
+        df = df.sort_values(by="p-value").iloc[:int(len(df) * self._BEST_MATCH_PERC / 100)]
+
         df[MOTIF_ID] = df.apply(
             lambda row: int(row["Query_ID"].split("-")[1]), axis=1
         )
         df[TF] = df.apply(
             lambda row: row["Target_ID"].split("&")[0][:4].upper(), axis=1
         )
-        return df.drop(["Query_ID", "Target_ID"], axis=1)
+        return df.drop(["Query_ID", "Target_ID", "p-value"], axis=1)
 
     def _read_contrib_scores(
         self,
