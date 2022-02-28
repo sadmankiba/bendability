bfc292b426f6dc87a98655931bb5eea8bab87b87
sadmankiba <sadmankiba@gmail.com>
2022-02-28 12:51:05 +0600

    Create file with bndrs fanc score perc 50

--- hic/fanc.sh
+++ hic/fanc.sh
@@ -5,11 +5,14 @@ START_KB=180
 END_KB=280
 WINDOW_KB=1
 
-
+# Plot Hi-C map
 fancplot -o figures/chr${CHROMOSOME}_${START_KB}kb_${END_KB}kb_fanc.png \
     ${CHROMOSOME}:${START_KB}kb-${END_KB}kb -p triangular ${DATA}@${RES} -m 50000 -vmin 0 -vmax 50
 
+# Calculate insulation score and output into a file
 INSULATION_FILE=data/generated_data/chr${CHROMOSOME}_${START_KB}kb_${END_KB}kb.insulation 
 fanc insulation ${DATA}@${RES} ${INSULATION_FILE} \
     -r ${CHROMOSOME}:${START_KB}kb-${END_KB}kb -o bed -w 1000 2000 5000 10000 25000
+
+# Identify boundaries and write in a file
 fanc boundaries ${INSULATION_FILE}_${WINDOW_KB}kb.bed ${INSULATION_FILE}_${WINDOW_KB}kb_boundaries
